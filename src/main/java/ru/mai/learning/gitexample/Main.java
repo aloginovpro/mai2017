b1181fd205a64b65f2871fbc7cfc8d8665f558c5
a.loginov <a.loginov@zmeke.com>
2018-02-03 16:00:53 +0300

    Git example - feature3

--- src/main/java/ru/mai/learning/gitexample/Main.java
+++ src/main/java/ru/mai/learning/gitexample/Main.java
@@ -3,8 +3,12 @@ package ru.mai.learning.gitexample;
 public class Main {
 
     public static void main(String[] args) {
-        int answer = 42;
+        int answer = getAnswer();
         System.out.println("my answer is: " + answer);
+    }
+
+    static int getAnswer() {
+        return 42;
     }
 
 }
