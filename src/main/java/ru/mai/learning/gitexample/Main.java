8885c33f9cb9417421afc8b25bca46239630c795
a.loginov <a.loginov@zmeke.com>
2018-02-03 15:44:24 +0300

    Git example 1

---
+++ src/main/java/ru/mai/learning/gitexample/Main.java
@@ -0,0 +1,10 @@
+package ru.mai.learning.gitexample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int answer = 42;
+        System.out.println("answer: " + answer);
+    }
+
+}
